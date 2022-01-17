18802754d8cab2a04a2cf6d657f94e7831f51f89
Thisseas X <thisseasx@yahoo.com>
2022-01-17 16:58:14 +0200

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,74 @@
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import passport from 'passport';
+import JwtCookieComboStrategy from 'passport-jwt-cookiecombo';
+import { Strategy as LocalStrategy } from 'passport-local';
+
+const SECRET = 's0m3s3cr3t';
+
+passport
+  .use(
+    new JwtCookieComboStrategy(
+      {
+        secretOrPublicKey: SECRET,
+        jwtCookieName: 'authToken',
+      },
+      (payload, done) => {
+        return done(null, payload.user);
+      },
+    ),
+  )
+  .use(
+    new LocalStrategy((username, password, done) => {
+      done(null, { username, password });
+    }),
+  );
+
+const app = express();
+
+app
+  .use(cookieParser(SECRET))
+  .use(bodyParser.json())
+  .use(passport.initialize())
+  .post('/login', passport.authenticate('local', { session: false }), (req, res) => {
+    const user = req.user;
+
+    // Create auth token
+    const token = jwt.sign({ user }, SECRET);
+
+    // Set the cookie
+    res.cookie('authToken', token, {
+      httpOnly: true, // only visible to the server (not document.cookie)
+      sameSite: true,
+      signed: true,
+      secure: true,
+    });
+
+    res.json({ token });
+  })
+  .get(
+    '/logout',
+    passport.authenticate('jwt-cookiecombo', { session: false }),
+    (req, res) => {
+      console.log(req.logout()); // clears req.user
+      res.clearCookie('authToken'); // clears the cookie
+      res.send('logged out');
+    },
+  )
+  .get(
+    '/',
+    passport.authenticate('jwt-cookiecombo', { session: false }),
+    (req, res) => {
+      res.cookie('unsigned-cookie', '123');
+
+      console.log(req.cookies);
+      console.log(req.signedCookies);
+      console.log(req.user);
+      console.log(req.isAuthenticated());
+
+      res.send('hello');
+    },
+  )
+  .listen(4000);
